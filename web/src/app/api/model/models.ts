43d774698af69eed4d59c766993e313e3b4bdb5c
Benjamin Kuen <kuen@devstorage.eu>
2023-12-26 20:42:52 +0100

    Add angular api client

---
+++ web/src/app/api/model/models.ts
@@ -0,0 +1,7 @@
+export * from './device';
+export * from './deviceKind';
+export * from './findDevices200Response';
+export * from './operatingSystem';
+export * from './protectionStatus';
+export * from './registerDevice200Response';
+export * from './registerDeviceRequest';
